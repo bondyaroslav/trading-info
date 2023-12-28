f33f55bda55c0864ada68e5dc8a60a36c3fbfa0a
bondyaroslav <bondy2526@gmail.com>
2023-12-28 23:42:28 +0200

    save

---
+++ src/components/Chart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {createChart} from "lightweight-charts";
+
+const Chart = () => {
+    return (
+        <div className={"container"}>
+
+        </div>
+    );
+};
+
+export default Chart;
