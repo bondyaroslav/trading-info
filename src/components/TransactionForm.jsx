09e780f5785e93fe1c79c984d0123cc47190b6d3
bondyaroslav <bondy2526@gmail.com>
2024-01-08 16:26:43 +0200

    fix(TransactionForm): empty url parameter bug

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -3,34 +3,44 @@ import constants from "../constants"
 import {useDispatch} from "react-redux"
 import {setOrdersAC} from "../store/ordersReducer"
 import styles from "./TransactionForm.module.css"
-import Select from "../UI/select/Select";
-import Input from "../UI/input/Input";
+import Select from "../UI/select/Select"
+import Input from "../UI/input/Input"
 
 const TransactionForm = () => {
     const dispatch = useDispatch()
-    const [currencyPairs, setCurrencyPairs] = useState(null)
-    const [strategyType, setStrategyType] = useState(null)
-    const [transactionType, setTransactionType] = useState(null)
-    const [capitalSize, setCapitalSize] = useState(null)
-    const [creditLeveraging, setCreditLeveraging] = useState(null)
-    const [startDate, setStartDate] = useState(null)
-    const [endDate, setEndDate] = useState(null)
+    const [currencyPairs, setCurrencyPairs] = useState("BNBUSDT")
+    const [strategyType, setStrategyType] = useState("martingale")
+    const [transactionType, setTransactionType] = useState("isolatedMargin")
+    const [capitalSize, setCapitalSize] = useState("")
+    const [creditLeveraging, setCreditLeveraging] = useState("")
+    const [startDate, setStartDate] = useState("")
+    const [endDate, setEndDate] = useState("")
 
     const fetchOrdersData = () => {
-        const url = `${constants.url}`
-            + `&currencyPairs=${currencyPairs}`
-            + `&strategyType=${strategyType}`
-            + `&transactionType=${transactionType}`
-            + `&capitalSize=${capitalSize}`
-            + `&creditLeveraging=${creditLeveraging}`
-            + `&startDate=${startDate}`
-            + `&endDate=${endDate}`
-        console.log(url)
-        fetch(url)
-            .then( response => response.json() )
-            .then( json => { if (json !== null) {
-                dispatch(setOrdersAC(json))
-            }})
+        if (
+            currencyPairs !== "" &&
+            strategyType !== "" &&
+            transactionType !== "" &&
+            capitalSize !== "" &&
+            creditLeveraging !== "" &&
+            startDate !== "" &&
+            endDate !== ""
+        ) {
+            const url = `${constants.url}`
+                + `&currencyPairs=${currencyPairs}`
+                + `&strategyType=${strategyType}`
+                + `&transactionType=${transactionType}`
+                + `&capitalSize=${capitalSize}`
+                + `&creditLeveraging=${creditLeveraging}`
+                + `&startDate=${startDate}`
+                + `&endDate=${endDate}`
+            console.log(url)
+            fetch(url)
+                .then( response => response.json() )
+                .then( json => { if (json !== null) {
+                    dispatch(setOrdersAC(json))
+                }})
+        } else console.log("error")
     }
 
     const handleCurrencyPairsChange = (newValue) => {setCurrencyPairs(newValue)}
@@ -48,11 +58,11 @@ const TransactionForm = () => {
             alignItems: "center",
             height: 100,
         }}>
-            <Select selectName={"currency pairs"} selectValues={constants.currencyPairs} onChange={handleCurrencyPairsChange}/>
+            <Select selectName={"currency pairs"} value={currencyPairs} selectValues={constants.currencyPairs} onChange={handleCurrencyPairsChange}/>
 
             <div className={styles.parameter}>
-                <Select selectName={"strategy type"} selectValues={constants.strategyType} onChange={handleStrategyTypeChange}/>
-                <Select selectName={"transaction type"} selectValues={constants.transactionType} onChange={handleTransactionTypeChange}/>
+                <Select selectName={"strategy type"} value={strategyType} selectValues={constants.strategyType} onChange={handleStrategyTypeChange}/>
+                <Select selectName={"transaction type"} value={transactionType} selectValues={constants.transactionType} onChange={handleTransactionTypeChange}/>
             </div>
 
             <div className={styles.parameter}>
@@ -65,8 +75,7 @@ const TransactionForm = () => {
                 <Input inputType={"date"} inputName={"end date"} onChange={handleEndDate}/>
             </div>
 
-            <button onClick={fetchOrdersData}>submit
-            </button>
+            <button onClick={fetchOrdersData}>submit</button>
         </div>
     )
 }
